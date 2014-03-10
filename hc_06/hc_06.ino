609ef85ba3e762dbbf4625ffb41cedc50c97cea6
Iver Egge <iverasp@gmail.com>
2014-03-10 19:20:57 +0100

    made a comment about the electrical connection

--- hc_06/hc_06.ino
+++ hc_06/hc_06.ino
@@ -1,6 +1,11 @@
 /*
 Software for running AT-commands on the HC-06.
 Almost a copy of the code available at http://arduino.cc/en/Reference/SoftwareSerial
+
+Connection:
+TX of HC-06 to pin 10 of the Arduino UNO.
+RX of the HC-06 to pin 11 of the Arduino UNO through
+a voltage divider (i.e. 18k and 33k resistors for dropping 5V to ~3.3V).
 */
 
 #include <SoftwareSerial.h>
