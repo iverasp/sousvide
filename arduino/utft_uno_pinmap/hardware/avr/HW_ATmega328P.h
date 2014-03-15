131180703ad14982e780819fff39bec9f1a45ea8
Iver Egge <iverasp@gmail.com>
2014-03-15 13:33:49 +0100

    working code with different pins

--- arduino/utft_uno_pinmap/hardware/avr/HW_ATmega328P.h
+++ arduino/utft_uno_pinmap/hardware/avr/HW_ATmega328P.h
@@ -1,4 +1,16 @@
 // *** Hardwarespecific functions ***
+/* Modified by Iver Egge to work with different pins
+on the MCU. The connection to the LCD becomes as follows:
+DB0 -> pin 13
+DB1 -> pin 12
+DB2 -> pin 11
+DB3 -> pin 10
+DB4 -> pin 9
+DB5 -> pin 8
+DB6 -> pin 7
+DB7 -> pin 6
+*/
+
 void UTFT::_hw_special_init()
 {
 }
@@ -66,9 +78,76 @@ void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 		pulse_low(P_SCL, B_SCL);
 		break;
 	case 8:
-		PORTD = VH;
+		//PORTD = VH;
+        if (VH & 0x01)
+            PORTB |= (1<<5);
+        else
+            PORTB &= ~(1<<5);
+        if (VH & 0x02)
+            PORTB |= (1<<4);
+        else
+            PORTB &= ~(1<<4);
+        if (VH & 0x04)
+            PORTB |= (1<<3);
+        else
+            PORTB &= ~(1<<3);
+        if (VH & 0x08)
+            PORTB |= (1<<2);
+        else
+            PORTB &= ~(1<<2);
+        if (VH & 0x10)
+            PORTB |= (1<<1);
+        else
+            PORTB &= ~(1<<1);
+        if (VH & 0x20)
+            PORTB |= (1<<0);
+        else
+            PORTB &= ~(1<<0);
+        if (VH & 0x40)
+            PORTD |= (1<<7);
+        else
+            PORTD &= ~(1<<7);
+        if (VH & 0x80)
+            PORTD |= (1<<6);
+        else
+            PORTD &= ~(1<<6);
+         
 		pulse_low(P_WR, B_WR);
-		PORTD = VL;
+
+		//PORTD = VL;
+        if (VL & 0x01)
+            PORTB |= (1<<5);
+        else
+            PORTB &= ~(1<<5);
+        if (VL & 0x02)
+            PORTB |= (1<<4);
+        else
+            PORTB &= ~(1<<4);
+        if (VL & 0x04)
+            PORTB |= (1<<3);
+        else
+            PORTB &= ~(1<<3);
+        if (VL & 0x08)
+            PORTB |= (1<<2);
+        else
+            PORTB &= ~(1<<2);
+        if (VL & 0x10)
+            PORTB |= (1<<1);
+        else
+            PORTB &= ~(1<<1);
+        if (VL & 0x20)
+            PORTB |= (1<<0);
+        else
+            PORTB &= ~(1<<0);
+        if (VL & 0x40)
+            PORTD |= (1<<7);
+        else
+            PORTD &= ~(1<<7);
+        if (VL & 0x80)
+            PORTD |= (1<<6);
+        else
+            PORTD &= ~(1<<6); 
+
 		pulse_low(P_WR, B_WR);
 		break;
 	case 16:
@@ -92,7 +171,12 @@ void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 
 void UTFT::_set_direction_registers(byte mode)
 {
-	DDRD = 0xFF;
+	//DDRD = 0xFF;
+    DDRB = 0xFF; // using all pins on port B
+    // additionally use these two pins on port D
+    pinMode(7, OUTPUT);
+    pinMode(6, OUTPUT);
+
 	if (mode==16)
 	{
 		DDRB |= 0x3F;
